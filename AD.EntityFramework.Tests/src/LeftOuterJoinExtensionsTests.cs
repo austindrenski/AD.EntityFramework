3a3f35489647bc447dbaab71e6b023d4f0c03b6b
Austin Drenski <austin.drenski@gmail.com>
2017-02-04 13:57:48 -0500

    commit

---
+++ AD.EntityFramework.Tests/src/LeftOuterJoinExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿//using System.Linq;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace AD.EntityFramework.Tests
+//{
+//    [TestClass]
+//    public class LeftOuterJoinExtensionsTests
+//    {
+//        [TestMethod]
+//        public void LeftOuterJoinTest()
+//        {
+//            // Arrange
+//            ITradeContext context = MockContextFactory.CreateMockTradeContext();
+//            var imports = context.DatawebImports(new string[] { "2015" });
+//            var names = context.CountryCodeCrosswalk;
+
+//            // Act
+//            var test = from a in imports
+//                       from b in names.LeftOuterJoin(x => x.CountryCodeDataweb == a.CountryCode)
+//                       select new
+//                       {
+//                           a.CountryCode,
+//                           b.Iso
+//                       };
+
+//            // Assert
+//            Assert.IsTrue(test.ReduceExpression().ToList().Count == 4);
+//        }
+//    }
+//}
